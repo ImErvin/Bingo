64d5750036fa3b032f626deae95fa45f79a0d9b5
Tangqi Feng <kylezete@gmail.com>
2019-01-11 09:27:33 +0000

    add game object

---
+++ game.js
@@ -0,0 +1,40 @@
+// game object
+function Game() {
+    this.players = [];
+    this.startTime = 0;
+    this.endTime = 0;
+    this.winner = null;
+    this.isplaying = false;
+}
+
+Game.prototype.addPlayer = (user) => {
+    this.players.push(user);
+}
+
+Game.prototype.clearPlayer = () => {
+    this.players = null;
+}
+
+Game.prototype.start = () => {
+    this.isplaying = true;
+    this.startTime = Date.now();
+}
+
+Game.prototype.stop = () => {
+    this.isplaying = false;
+    this.endTime = Date.now();
+}
+
+Game.prototype.gameTime = () => {
+    // return seconds
+    return Math.floor((this.endTime - this.startTime)/1000);
+}
+
+var g = new Game();
+g.start();
+setTimeout(() => {
+    g.stop();
+    console.log(g.gameTime());
+}, 2000);
+
+//console.log(g.gameTime());
